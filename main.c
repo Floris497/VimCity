5584e4673b87a4db0bf05769457f6fdaa622acfc
Floris Fredrikze <floris@noxbox.nl>
2020-01-07 17:06:56 +0100

    feature: added game loop delay for 60fps

--- main.c
+++ main.c
@@ -5,26 +5,39 @@
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 
-gameLoop(SDL_Window *window, SDL_Surface *screenSurface);
+int gameLoop(SDL_Window *window, SDL_Surface *screenSurface);
+
+#define FRAME_DURATION (SDL_GetPerformanceFrequency()/60)
 
 int main(int argc, char* args[]) {
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
     
+    long double duration = (SDL_GetPerformanceFrequency()/60.0);
+    
     if(SDL_Init( SDL_INIT_VIDEO) < 0) {
         printf("SDL not loaded");
     } else {
         //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN, SDL_WINDOW_ALLOW_HIGHDPI);
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
         if(window == NULL) {
             printf("could not make window");
         } else {
             screenSurface = SDL_GetWindowSurface(window);
             bool quit = false;
+            
+            long double delta;
+            Uint64 now, end, taken;
             while (!quit) {
+                now = SDL_GetPerformanceCounter();
                 quit = !gameLoop(window, screenSurface);
+                end = SDL_GetPerformanceCounter();
+                taken = end - now;
+                delta = round(duration - taken) / (SDL_GetPerformanceFrequency() / 1000);
+                SDL_Log("Taken: %llu, %Lf\n", taken / SDL_GetPerformanceFrequency(), delta);
+                if (delta > 0)
+                    SDL_Delay(round(delta));
             }
-            Uint64 now = SDL_GetPerformanceCounter();
             
             SDL_DestroyWindow(window);
             SDL_Quit();

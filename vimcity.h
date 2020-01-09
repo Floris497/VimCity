fe14652ea59d4889660236c76f3a79ffb5e40a84
Floris Fredrikze <floris@noxbox.nl>
2020-01-09 19:26:34 +0100

    feature: added structs for gamestate

--- vimcity.h
+++ vimcity.h
@@ -12,6 +12,12 @@
 #define SCREEN_HEIGHT 720
 
 typedef struct s_color t_color;
+typedef struct s_game t_game;
+typedef struct s_map t_map;
+
+typedef struct s_point t_point;
+typedef struct s_size t_size;
+typedef struct s_rect t_rect;
 
 struct s_color {
     Uint8 r;
@@ -19,6 +25,30 @@ struct s_color {
     Uint8 b;
     Uint8 a;
 };
+
+struct s_game {
+    t_map map;
+    t_size screenSize;
+}
+
+struct s_map {
+    field *field;
+    t_size size;
+}
+
+struct s_point {
+    Int64 x;
+    Int64 y;
+}
+struct s_size {
+    Uint64 width;
+    Uint64 heigt:
+}
+struct s_rect {
+    t_point origin;
+    t_size size;
+}
+
 
 int gameLoop(SDL_Renderer *renderer);
 
